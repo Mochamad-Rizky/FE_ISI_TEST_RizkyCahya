557e5cb4c0e67fd2f68869598cecfea53dd53450
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2025-03-07 17:29:15 +0700

    refactor: Remove commented-out keyframes, animations, and screens from Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -45,34 +45,6 @@ const config: Config = {
         base: '500',
         heading: '700',
       },
-      // keyframes: {
-      //   'accordion-down': {
-      //     from: { height: '0' },
-      //     to: { height: 'var(--radix-accordion-content-height)' },
-      //   },
-      //   'accordion-up': {
-      //     from: { height: 'var(--radix-accordion-content-height)' },
-      //     to: { height: '0' },
-      //   },
-      //   marquee: {
-      //     '0%': { transform: 'translateX(0%)' },
-      //     '100%': { transform: 'translateX(-100%)' },
-      //   },
-      //   marquee2: {
-      //     '0%': { transform: 'translateX(100%)' },
-      //     '100%': { transform: 'translateX(0%)' },
-      //   },
-      // },
-      // animation: {
-      //   'accordion-down': 'accordion-down 0.2s ease-out',
-      //   'accordion-up': 'accordion-up 0.2s ease-out',
-      //   marquee: 'marquee 15s linear infinite',
-      //   marquee2: 'marquee2 15s linear infinite',
-      // },
-      // screens: {
-      //   w900: { raw: '(max-width: 900px)' },
-      //   w500: { raw: '(max-width: 500px)' },
-      // },
     },
   },
   plugins: [tailwindAnimate],
